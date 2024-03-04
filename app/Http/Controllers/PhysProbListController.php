e5815183b4e8a7d15ca2c83211157b49da30bac5
kazumi38 <ixx.kzm.3865@gmail.com>
2024-03-04 09:30:07 +0900

    データベースのデータをすべて表示する機能の追加

--- app/Http/Controllers/PhysProbListController.php
+++ app/Http/Controllers/PhysProbListController.php
@@ -4,7 +4,55 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
+use App\Models\Member;
+
 class PhysProbListController extends Controller
 {
-    //
+    public function index()
+    {
+        $phys_prob_lists = DB::table('phys_prob_lists')
+            -> select(
+                'Workbook',
+                'ProbNo',
+                'Field',
+                'Class',
+                'Level',
+                'UsedDay',
+                'updated_at',
+                'created_at')
+            ->get();
+
+        return view('phys_prob_lists/index', compact('phys_prob_lists'));
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PhysProbListController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/allview', [PhysProbListController::class, 'index']
+)->name('phys_prob_lists.index');
